4d985464a8cfae3a06da908dbc3e048cf5d6935e
Ameer-Hamza7 <91970090+Ameer-Hamza7@users.noreply.github.com>
2024-06-01 13:01:54 +0500

    adding shared.sh

--- deploy_engine/tasks.py
+++ deploy_engine/tasks.py
@@ -7,6 +7,6 @@ from celery import shared_task
 def deployer_script():
   # Access instance data if needed for the script
   # ...
-  result = run("./shared.sh", shell=True)
+  result = run("sh shared.sh", check=True)
   print('results .............', result)
   # Handle success or failure based on result.returncode
