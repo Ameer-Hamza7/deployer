2e05dfb876c6707138e2bea93a971d9c0632c52d
Ameer-Hamza7 <91970090+Ameer-Hamza7@users.noreply.github.com>
2024-06-08 17:25:20 +0500

    Updating Jobs

--- jobs/shared.sh
+++ jobs/shared.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo 'Hello world'
 docker login
-cd /root/xtremeanalytix/XtremeAnalytixBridge/xtremeanalytix-data-bridge && docker-cmpose up
+$DATA_BRIDGE_TAG_VERSION=version-0.0.4  
+cd /root/xtremeanalytix/XtremeAnalytixBridge/xtremeanalytix-data-bridge && docker-cmpose up --force-recreate --always-recreate-deps --remove-orphans 
