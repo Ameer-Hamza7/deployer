9d4363b4c27069ef51453af39a5ad85f993e1688
Ameer-Hamza7 <91970090+Ameer-Hamza7@users.noreply.github.com>
2024-06-08 19:31:16 +0500

    main job file

--- deploy_jobs.py
+++ deploy_jobs.py
@@ -1,23 +1,25 @@
-import psycopg2
+# import psycopg2
 
 
-# Establishing a db connection
-conn = psycopg2.connect(database = "deployer", 
-                        user = "postgres", 
-                        host= "158.220.114.235",
-                        password = "postgres",
-                        port = 6432)
+# # Establishing a db connection
+# conn = psycopg2.connect(database = "deployer", 
+#                         user = "postgres", 
+#                         host= "158.220.114.235",
+#                         password = "postgres",
+#                         port = 6432)
 
-cur = conn.cursor()
+# cur = conn.cursor()
 
-sql = "SELECT * FROM public.deploy_engine_appdeploymenthistory WHERE build_status = 'Active';"
-cur.execute(sql)
-rows = cur.fetchall()
-conn.commit()
-conn.close()
-for row in rows:
-    print(row)
+# sql = "SELECT * FROM public.deploy_engine_appdeploymenthistory WHERE build_status = 'Active';"
+# cur.execute(sql)
+# rows = cur.fetchall()
+# conn.commit()
+# conn.close()
+# for row in rows:
+#     print(row)
 
-import subprocess
-output = subprocess.call(["source jobs/shared.sh"])
-print(output)
+# import subprocess
+# output = subprocess.call(["source jobs/shared.sh"])
+# print(output)
+
+print("Hello World")
--- jobs/shared.sh
+++ jobs/shared.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-echo 'Hello world'
-docker login
-$DATA_BRIDGE_TAG_VERSION=version-0.0.4  
-cd /root/xtremeanalytix/XtremeAnalytixBridge/xtremeanalytix-data-bridge && docker-cmpose up --force-recreate --always-recreate-deps --remove-orphans 
+docker login  
+cd /root/xtremeanalytix/XtremeAnalytixBridge/xtremeanalytix-data-bridge && docker-cmpose up --force-recreate --always-recreate-deps --remove-orphans
+docker image prune
