745d7641bfead09e57c112a3eda756407b8487a0
Ameer-Hamza7 <91970090+Ameer-Hamza7@users.noreply.github.com>
2024-06-08 09:37:23 +0500

    Adding postgres config

--- main/settings.py
+++ main/settings.py
@@ -79,11 +79,14 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'deployer',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '158.220.114.235',
+        'PORT': '6432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
