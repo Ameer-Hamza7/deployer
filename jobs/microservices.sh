b25e0ad3b411d765d6f423401044c90c4a43f001
Ameer-Hamza7 <91970090+Ameer-Hamza7@users.noreply.github.com>
2024-06-29 16:04:35 +0500

    docker workflow update

--- jobs/microservices.sh
+++ jobs/microservices.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker login
-cd /root/xtremeanalytix/deployer/deployer/jobs/microservices && docker-compose pull $1 && docker-compose up -d --force-recreate --always-recreate-deps
+cd /root/xtremeanalytix/deployer/deployer/jobs/microservices && docker-compose down && docker-compose pull && docker-compose up -d --force-recreate --always-recreate-deps
 # docker image prune -a -f
