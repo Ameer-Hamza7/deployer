7a9c15445c26e39b69a54bdf510bddfd5a19d7ce
Ameer-Hamza7 <91970090+Ameer-Hamza7@users.noreply.github.com>
2024-06-29 14:33:20 +0500

    remove pruning

--- jobs/visualization.sh
+++ jobs/visualization.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker login
 cd /root/xtremeanalytix/deployer/deployer/jobs/visualization && docker-compose pull $1 && docker-compose up -d --force-recreate --always-recreate-deps
-docker image prune -a -f
+# docker image prune -a -f
