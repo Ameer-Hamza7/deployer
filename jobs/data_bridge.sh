c62f83733c83e6e0aaed075a634afd4a6578c7f5
MinhajMateen <155780228+MinhajMateen@users.noreply.github.com>
2024-07-13 17:06:08 +0500

    making correction in .sh file

--- jobs/data_bridge.sh
+++ jobs/data_bridge.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker login
-cd /root/xtremeanalytix/XtremeAnalytixBridge/xtremeanalytix-data-bridge && dockercompose pull && dockercompose up -d
+cd /root/xtremeanalytix/XtremeAnalytixBridge/xtremeanalytix-data-bridge && docker compose pull && docker compose up -d
 # docker image prune -a -f
--- jobs/visualization.sh
+++ jobs/visualization.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker login
-cd /root/xtremeanalytix/deployer/deployer/jobs/visualization && dockercompose down && dockercompose pull && dockercompose up -d
+cd /root/xtremeanalytix/deployer/deployer/jobs/visualization && docker compose down && docker compose pull && docker compose up -d
 # docker image prune -a -f
