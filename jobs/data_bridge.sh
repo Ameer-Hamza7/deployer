a2dd87188e3d0951d71c93a4322e183049a8f00e
root <root@server-67-217-59-94.da.direct>
2024-07-13 05:56:32 +0000

    Production deployment changes

--- jobs/data_bridge.sh
+++ jobs/data_bridge.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker login
-cd /root/xtremeanalytix/XtremeAnalytixBridge/xtremeanalytix-data-bridge && docker-compose pull && docker-compose up -d --force-recreate --always-recreate-deps
+cd /root/xtremeanalytix/XtremeAnalytixBridge/xtremeanalytix-data-bridge && docker compose pull && docker compose up -d 
 # docker image prune -a -f
--- jobs/microservices.sh
+++ jobs/microservices.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker login
-cd /root/xtremeanalytix/deployer/deployer/jobs/microservices && docker-compose down && docker-compose pull && docker-compose up -d
+cd /root/xtremeanalytix/deployer/deployer/jobs/microservices && docker compose down && docker compose pull && docker compose up -d
 # docker image prune -a -f
